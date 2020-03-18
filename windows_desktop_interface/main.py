392c1875145c561659f34e9666b30e65bfc4ba55
Leonardo Lucio Custode <leonardo@localhost.localdomain>
2020-03-18 18:37:51 +0100

    Added logo

--- windows_desktop_interface/main.py
+++ windows_desktop_interface/main.py
@@ -12,10 +12,12 @@ import json
 import threading
 import traceback
 from reportlab.pdfgen import canvas
+from response_ui import Response
 from classifiers import TFClassifier
+from utilities import customize_report, export_to_pdf
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QDialog
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot, QThreadPool, QRunnable, QObject, pyqtSignal
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import pyqtSlot, QThreadPool, QRunnable, QObject, pyqtSignal, Qt
 
 
 class WorkerSignals(QObject):
@@ -35,15 +37,9 @@ class Worker(QRunnable):
     @pyqtSlot()
     def run(self):
         """ Runs the thread """
-        input_ = {'avi_files': [], 'other_files':[]}
+        input_ = {'working_dir': self.dir_path}
 
         try:
-            for file in os.listdir(self.dir_path):
-                if ".avi" in file:
-                    input_['avi_files'].append(os.path.join(self.dir_path, file))
-                else:
-                    input_['other_files'].append(os.path.join(self.dir_path, file))
-
             result = self.classifier.predict(json.dumps(input_))
             self.signals.result.emit(result)  # Return the result of the processing
         except:
@@ -57,15 +53,15 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Application'
-        self.left = 16
-        self.top = 15
-        self.width = 640
-        self.height = 480
+        self.left = 50
+        self.top = 50
+        self.width = 320
+        self.height = 180
         self.init_ui()
 
         self.threadpool = QThreadPool()
         self._dialogs = []
-        self._relevant_fields = ['paziente', 'esito classificazione', 'dettagli classificazione']
+        self._relevant_fields = ['name']
 
     def init_ui(self):
         """
@@ -75,10 +71,19 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # Create a layout and put the button into it
+        # Create a layout and put the logo and a button into it
         layout = QVBoxLayout()
-        button = QPushButton("Seleziona cartella paziente")
+        
+        image_label = QLabel("")
+        pixmap = QPixmap('resources/logo_unitn.png')
+        pixmap = pixmap.scaled(200, 80, Qt.KeepAspectRatio)
+        image_label.setPixmap(pixmap)
+        
+
+        button = QPushButton("Select patient's folder")
         button.clicked.connect(self.choose_file)
+
+        layout.addWidget(image_label, alignment=Qt.AlignRight)
         layout.addWidget(button)
 
         self.setLayout(layout)
@@ -87,7 +92,7 @@ class App(QWidget):
     @pyqtSlot()
     def choose_file(self):
         """ Opens the file chooser and starts processing in a separate thread """
-        data_path = QFileDialog.getExistingDirectory(self, "Seleziona cartella")
+        data_path = QFileDialog.getExistingDirectory(self, "Select folder")
 
         if len(data_path) == 0:
             return
@@ -98,54 +103,23 @@ class App(QWidget):
     def process_result(self, task):
         """ Retrieves the output of a task """
         task_dict = json.loads(task)
-        self.show_dialog(task_dict)
-        self.create_pdf(task_dict)
-    
-    def create_pdf(self, task_dict):
-        """ Creates a pdf file in the folder containing the AVI files """
-        inch = 72
-        title = "Referto {}".format(task_dict['paziente'])
-        output_path = os.path.join(task_dict['avi_files'][0].split("\\")[0], title + ".pdf")
+        html = customize_report(task)
 
-        try:
-            # Delete the file if it exists
-            if os.path.exists(output_path):
-                os.remove(output_path)
-
-            c = canvas.Canvas(output_path, pagesize=(8.5 * inch, 11 * inch))
-            c.setStrokeColorRGB(0,0,0)
-            c.setFillColorRGB(0,0,0)
-
-            v = 10 * inch
-
-            # Write the title
-            c.setFont("Helvetica", 24)
-            c.drawString(1 * inch, v, title)
-            v -= 12 * 2
-
-            # Write the details
-            c.setFont("Helvetica", 12)
-
-            fields = self._relevant_fields
-            for field in fields:
-                c.drawString(1 * inch, v, "{}: {}".format(field.capitalize(), task_dict[field]))
-                v -= 12
-            c.save()    
-        except:
-            self.show_alert({'paziente': task_dict['paziente'], 'errore': 'Si è verificato un errore durante il salvataggio del referto.' +
-                              'Verificare che il file non sia già esistente ed aperto.'}, ['paziente', 'errore'])
+        # Show output
+        response = Response(html)
+        self._dialogs.append(response)
+        # Save to PDF
 
     def show_alert(self, task_dict, fields):
-        """ Shows a generic alert. task_dict is a dictionary containing at least the "paziente" field and the one contained in fields """
+        """ Shows a generic alert. task_dict is a dictionary containing at least the "name" field and the one contained in fields """
         new_window = QDialog()
-        new_window.setWindowTitle("Esito paziente {}".format(task_dict['paziente']))
+        new_window.setWindowTitle("{}".format(task_dict['name']))
 
         layout = QVBoxLayout()
 
         for field in fields:
             label = QLabel("{}: {}".format(field.capitalize(), task_dict[field]))
             layout.addWidget(label)
-
 
         new_window.setLayout(layout)
         self._dialogs.append(new_window)
@@ -162,10 +136,6 @@ class App(QWidget):
             self._dialogs.remove(tr)
             
             
-    def show_dialog(self, task_dict):
-        """ Shows a summary (equal to the pdf) containing only the relevant fields """
-        self.show_alert(task_dict, self._relevant_fields)
-
 
 if __name__ == '__main__':
     app = QApplication([""])
